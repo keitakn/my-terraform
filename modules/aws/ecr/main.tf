208cd39e7ef1e284a8f90c1523a743174378dad5
keitakn <keita.koga.work@gmail.com>
2020-01-31 11:11:24 +0900

    :sparkles: #21 grpcアプリケーション用のECRリポジトリを作成

--- modules/aws/ecr/main.tf
+++ modules/aws/ecr/main.tf
@@ -6,6 +6,14 @@ resource "aws_ecr_repository" "go_graphql" {
   name = "${terraform.workspace}-go-graphql"
 }
 
+resource "aws_ecr_repository" "golang_grpc_server" {
+  name = "${terraform.workspace}-golang-grpc-server"
+}
+
+resource "aws_ecr_repository" "golang_grpc_client" {
+  name = "${terraform.workspace}-golang-grpc-client"
+}
+
 locals {
   lifecycle_policy = <<EOF
   {
@@ -35,5 +43,15 @@ resource "aws_ecr_lifecycle_policy" "go_rest_api" {
 
 resource "aws_ecr_lifecycle_policy" "go_graphql" {
   repository = aws_ecr_repository.go_graphql.name
+  policy     = local.lifecycle_policy
+}
+
+resource "aws_ecr_lifecycle_policy" "golang_grpc_server" {
+  repository = aws_ecr_repository.golang_grpc_server.name
+  policy     = local.lifecycle_policy
+}
+
+resource "aws_ecr_lifecycle_policy" "golang_grpc_client" {
+  repository = aws_ecr_repository.golang_grpc_client.name
   policy     = local.lifecycle_policy
 }
