64ae9f9fc5a6a1f4359611bf1f1bfe10ddc9ea74
keitakn <keita.koga.work@gmail.com>
2020-02-02 22:42:22 +0900

    :sparkles: #21 EKS用のfargate profileを作成

--- modules/aws/eks/iam.tf
+++ modules/aws/eks/iam.tf
@@ -28,3 +28,23 @@ resource "aws_iam_role_policy_attachment" "attach_eks_service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_cluster_role.name
 }
+
+resource "aws_iam_role" "eks_fargate_role" {
+  name = "${terraform.workspace}-eks-fargate-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_eks_pod_execution_role" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.eks_fargate_role.name
+}
--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -21,3 +21,17 @@ resource "aws_eks_cluster" "eks_cluster" {
     aws_iam_role_policy_attachment.attach_eks_service_policy,
   ]
 }
+
+resource "aws_eks_fargate_profile" "golang_grpc_client" {
+  cluster_name           = aws_eks_cluster.eks_cluster.name
+  fargate_profile_name   = "${terraform.workspace}-golang-grpc-client"
+  pod_execution_role_arn = aws_iam_role.eks_fargate_role.arn
+  subnet_ids = [
+    var.vpc["subnet_private_1"],
+    var.vpc["subnet_private_2"],
+    var.vpc["subnet_private_3"],
+  ]
+  selector {
+    namespace = "golang-grpc-client"
+  }
+}
