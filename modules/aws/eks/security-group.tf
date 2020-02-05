2fc08cc38bd6640a971946ac2e010c969ef348ea
keitakn <keita.koga.work@gmail.com>
2020-02-05 20:02:29 +0900

    :sparkles: #21 ingress-controllerが利用するIAMロールを作成

--- modules/aws/eks/security-group.tf
+++ modules/aws/eks/security-group.tf
@@ -22,3 +22,36 @@ resource "aws_security_group" "eks_master" {
   }
 }
 
+resource "aws_security_group" "eks_pod" {
+  name        = "${terraform.workspace}-eks-pod"
+  description = "EKS pod Security Group"
+  vpc_id      = var.vpc["vpc_id"]
+
+  tags = {
+    Name = "${terraform.workspace}-eks-pod"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "eks_alb" {
+  name        = "${terraform.workspace}-eks-alb"
+  description = "EKS ALB Security Group"
+  vpc_id      = var.vpc["vpc_id"]
+
+  tags = {
+    Name = "${terraform.workspace}-eks-alb"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
