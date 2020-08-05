458e05692cfea1a2cb2a05c0b1fcb6ab284d4e75
keitakn <keita.koga.work@gmail.com>
2020-08-05 22:27:43 +0900

    :sparkles: #23 IDプールを構築

--- modules/aws/cognito/main.tf
+++ modules/aws/cognito/main.tf
@@ -1,5 +1,5 @@
-resource "aws_cognito_user_pool" "pool" {
-  name                     = "${terraform.workspace}-keitakn"
+resource "aws_cognito_user_pool" "user_pool" {
+  name                     = "${terraform.workspace}-keitakn-user-pool"
   auto_verified_attributes = ["email"]
 
   admin_create_user_config {
@@ -49,12 +49,75 @@ resource "aws_cognito_user_pool" "pool" {
   }
 }
 
+resource "aws_cognito_identity_pool" "id_pool" {
+  identity_pool_name               = "${terraform.workspace} keitakn IDPool"
+  allow_unauthenticated_identities = false
+}
+
+resource "aws_iam_role" "authenticated" {
+  name = "${terraform.workspace}-cognito-authenticated"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.id_pool.id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "authenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "authenticated" {
+  name = "${terraform.workspace}-authenticated-policy"
+  role = aws_iam_role.authenticated.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "mobileanalytics:PutEvents",
+        "cognito-sync:*",
+        "cognito-identity:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "id_pool" {
+  identity_pool_id = aws_cognito_identity_pool.id_pool.id
+
+  roles = {
+    "authenticated" = aws_iam_role.authenticated.arn
+  }
+}
+
 // https://github.com/keitakn/next-idaas
 resource "aws_cognito_user_pool_client" "next_idaas_client" {
   name                          = "${terraform.workspace}-next-idaas"
-  user_pool_id                  = aws_cognito_user_pool.pool.id
+  user_pool_id                  = aws_cognito_user_pool.user_pool.id
   generate_secret               = false
   prevent_user_existence_errors = "ENABLED"
   refresh_token_validity        = 30
-  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
 }
