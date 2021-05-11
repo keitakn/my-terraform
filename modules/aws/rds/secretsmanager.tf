fc0008a167f5b653b17948f21011491a913bc09b
keitakn <keita.koga.work@gmail.com>
2021-05-11 22:15:04 +0900

    :recycle: #51 一度消すと同じ名前で再定義する時に対応が必要なのでコメントを追加、あと名前も変更

--- modules/aws/rds/secretsmanager.tf
+++ modules/aws/rds/secretsmanager.tf
@@ -7,12 +7,14 @@ data "aws_secretsmanager_secret_version" "rds_secret" {
 }
 
 // RDS Proxyから利用する接続情報
-resource "aws_secretsmanager_secret" "rds_connection_info" {
-  name = "${terraform.workspace}/keitakn/rds-connection-info"
+// AWS Secrets Managerは一度削除すると7日間は同じ名前を再定義出来ない、対処法は下記を参照
+// https://aws.amazon.com/jp/premiumsupport/knowledge-center/delete-secrets-manager-secret/
+resource "aws_secretsmanager_secret" "rds_connection" {
+  name = "${terraform.workspace}/keitakn/rds-connection"
 }
 
-resource "aws_secretsmanager_secret_version" "rds_connection_info" {
-  secret_id = aws_secretsmanager_secret.rds_connection_info.id
+resource "aws_secretsmanager_secret_version" "rds_connection" {
+  secret_id = aws_secretsmanager_secret.rds_connection.id
 
   secret_string = jsonencode({
     username : aws_rds_cluster.rds_cluster.master_username
